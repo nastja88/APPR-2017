288bc2d99d29cad59665de4c136c08a3cd55688f
Nastja Kosir <nastja.kosir555@gmail.com>
2017-05-21 21:35:27 +0200

    vizualizacija

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -12,5 +12,11 @@ source(lib/uvozi.zemljevid.r)
 uvozi.zemljevid("http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/110m/cultural/ne_110m_admin_0_countries.zip",
                 "ne_110m_admin_0_countries/ne_110m_admin_0_countries")
 
-ggplot(tobak) + aes(x=leto, y=pojavnost, color=spol) + geom_point()
+ggplot(tobak) + aes(x=leto, y=pojavnost, color=spol) + geom_jitter()
+ggplot(filter(worldbank, worldbank$serija=="malarija")) + aes(x=leto, y=pojavnost) + geom_curve(xend=2015, yend=12000000)
+
+
+ggplot(alkohol) + aes(x=leto, y=poraba) + geom_jitter()
 ggplot(sifilis) + aes(x=leto, y=pojavnost) + geom_point()
+
+       
