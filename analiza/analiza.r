49a04a1fea9c333648f55f77e564d246ea040f17
Nastja Kosir <nastja.kosir555@gmail.com>
2017-07-08 19:50:07 +0200

    razvrščanje

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -1,11 +1,22 @@
 # 4. faza: Analiza podatkov
 
-podatki <- obcine %>% transmute(obcina, povrsina, gostota,
-                                gostota.naselij = naselja/povrsina) %>%
-  left_join(povprecja, by = "obcina")
-row.names(podatki) <- podatki$obcina
-podatki$obcina <- NULL
-
-# Število skupin
-n <- 5
-skupine <- hclust(dist(scale(podatki))) %>% cutree(n)
+alk <- filter(znacilnosti, leto == 2000, znacilnost == "alkohol") %>% rename(alkohol = pojavnost)
+alk$znacilnost <- NULL
+alk$leto <- NULL
+deb <- filter(znacilnosti, leto == 2000, znacilnost == "debelost") %>% rename(debelost = pojavnost)
+deb$znacilnost <- NULL
+deb$leto <- NULL
+den <- filter(znacilnosti, leto == 2000, znacilnost == "denar") %>% rename(denar = pojavnost)
+den$znacilnost <- NULL
+den$leto <- NULL
+pod <- filter(znacilnosti, leto == 2000, znacilnost == "podhranjenost") %>% rename(podhranjenost = pojavnost)
+pod$znacilnost <- NULL
+pod$leto <- NULL
+vod <- filter(znacilnosti, leto == 2000, znacilnost == "voda") %>% rename(voda = pojavnost)
+vod$znacilnost <- NULL
+vod$leto <- NULL
+zna <- full_join(alk, deb) %>% full_join(den) %>% full_join(pod) %>% full_join(vod)
+row.names(zna) <- zna$drzava
+zna$drzava <- NULL
+zna <- na.omit(zna)
+skup <- hclust(dist(scale(zna))) %>% cutree(5)
