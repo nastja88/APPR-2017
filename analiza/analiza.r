4a7b661cf980e841029d4c020f336b66a88c58b3
Nastja Kosir <nastja.kosir555@gmail.com>
2017-07-09 17:57:08 +0200

    korelacija med alkoholom, za zdravje namenjenim denarjem in dostopom do pitne vode ter razširjenostjo tuberkuloze

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -1,22 +1,58 @@
 # 4. faza: Analiza podatkov
 
-alk <- filter(znacilnosti, leto == 2000, znacilnost == "alkohol") %>% rename(alkohol = pojavnost)
+n <- 2000
+
+# države po skupinah glede na značilnosti
+alk <- filter(znacilnosti, leto == n, znacilnost == "alkohol") %>% rename(alkohol = pojavnost)
 alk$znacilnost <- NULL
 alk$leto <- NULL
-deb <- filter(znacilnosti, leto == 2000, znacilnost == "debelost") %>% rename(debelost = pojavnost)
-deb$znacilnost <- NULL
-deb$leto <- NULL
-den <- filter(znacilnosti, leto == 2000, znacilnost == "denar") %>% rename(denar = pojavnost)
+den <- filter(znacilnosti, leto == n, znacilnost == "denar") %>% rename(denar = pojavnost)
 den$znacilnost <- NULL
 den$leto <- NULL
-pod <- filter(znacilnosti, leto == 2000, znacilnost == "podhranjenost") %>% rename(podhranjenost = pojavnost)
-pod$znacilnost <- NULL
-pod$leto <- NULL
-vod <- filter(znacilnosti, leto == 2000, znacilnost == "voda") %>% rename(voda = pojavnost)
+vod <- filter(znacilnosti, leto == n, znacilnost == "voda") %>% rename(voda = pojavnost)
 vod$znacilnost <- NULL
 vod$leto <- NULL
-zna <- full_join(alk, deb) %>% full_join(den) %>% full_join(pod) %>% full_join(vod)
+
+zna <- full_join(alk, den) %>% full_join(vod)
+zna <- na.omit(zna)
+zna1<- zna
 row.names(zna) <- zna$drzava
 zna$drzava <- NULL
-zna <- na.omit(zna)
-skup <- hclust(dist(scale(zna))) %>% cutree(5)
+
+sk <- scale(zna) %>% kmeans(5, nstart = 1000)
+skupine <- data.frame(drzava = zna1$drzava, skupina = factor(sk$cluster))
+ggplot() + geom_polygon(data = svet %>% left_join(skupine, by = c("sovereignt" = "drzava")),
+                        aes(x = long, y = lat, group = group, fill = skupina)) + 
+  theme_void() + ggtitle("Države po podobnih značilnostih") + 
+  theme(plot.title = element_text(hjust = 0.5))
+
+# države po skupinah glede na bolezni
+hi <- filter(bolezni, leto == n, bolezen == "hiv") 
+hi$bolezen <- NULL
+hi$leto <- NULL
+mal <- filter(bolezni, leto == n, bolezen == "malarija") 
+mal$bolezen <- NULL
+mal$leto <- NULL
+tub <- filter(bolezni, leto == n, bolezen == "tuberkuloza")
+tub$bolezen <- NULL
+tub$leto <- NULL
+
+# zemljevid razširjenosti tuberkuloze
+ggplot() + geom_polygon(data = svet %>% left_join(tub, by = c("sovereignt" = "drzava")),
+                        aes(x = long, y = lat, group = group, fill = pojavnost)) + 
+  theme_void() + ggtitle("Razširjenost tuberkuloze") + 
+  theme(plot.title = element_text(hjust = 0.5))
+
+# zemljevid razširjenosti okuženih z virusom hiv
+ggplot() + geom_polygon(data = svet %>% left_join(hi, by = c("sovereignt" = "drzava")),
+                        aes(x = long, y = lat, group = group, fill = pojavnost)) + 
+  theme_void() + ggtitle("Razširjenost okuženih z virusom hiv") + 
+  theme(plot.title = element_text(hjust = 0.5))
+
+# zemljevid razširjenosti malarije
+ggplot() + geom_polygon(data = svet %>% left_join(mal, by = c("sovereignt" = "drzava")),
+                        aes(x = long, y = lat, group = group, fill = pojavnost)) + 
+  theme_void() + ggtitle("Razširjenost malarije") + 
+  theme(plot.title = element_text(hjust = 0.5))
+
+rm(alk, den, vod, hi, mal, tub)
