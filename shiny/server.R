06366c3913fd6037fda13c2d5a785bbb9ab6df54
Nastja Kosir <nastja.kosir555@gmail.com>
2017-07-12 16:07:16 +0200

    dodan zavihek v shiny

--- shiny/server.R
+++ shiny/server.R
@@ -1,9 +1,11 @@
 library(shiny)
 
 shinyServer(function(input, output){
-  output$zemljevid <- renderPlot({
+  
+  
+  output$bolezni <- renderPlot({
     if (input$bolezen == "AIDS") {
-      hi <- filter(bolezni, leto == n, bolezen == "hiv") 
+      hi <- filter(bolezni, leto == input$leto, bolezen == "hiv") 
       hi$bolezen <- NULL
       hi$leto <- NULL
       z <- ggplot() + geom_polygon(data = svet %>% left_join(hi, by = c("sovereignt" = "drzava")),
@@ -12,7 +14,7 @@ shinyServer(function(input, output){
         ggtitle("Zemljevid prikazuje razširjenost okuženih z virusom hiv") + 
         labs(caption = "Opomba: AIDS tu v resnici ne pomeni bolezni, pač pa označuje okužene z virusom hiv.")
     } else if (input$bolezen == "kolera") { 
-      kol <- filter(bolezni, leto == n, bolezen == "kolera")
+      kol <- filter(bolezni, leto == input$leto, bolezen == "kolera")
       kol$bolezen <- NULL
       kol$leto <- NULL
       z <- ggplot() + geom_polygon(data = svet %>% left_join(kol, by = c("sovereignt" = "drzava")),
@@ -20,7 +22,7 @@ shinyServer(function(input, output){
                                        fill = pojavnost)) + 
         ggtitle("Zemljevid prikazuje razširjenost kolere")
     } else if (input$bolezen == "malarija") {
-      mal <- filter(bolezni, leto == n, bolezen == "malarija") 
+      mal <- filter(bolezni, leto == input$leto, bolezen == "malarija") 
       mal$bolezen <- NULL
       mal$leto <- NULL
       z <- ggplot() + geom_polygon(data = svet %>% left_join(mal, by = c("sovereignt" = "drzava")),
@@ -28,15 +30,16 @@ shinyServer(function(input, output){
                                        fill = pojavnost)) + 
         ggtitle("Zemljevid prikazuje razširjenost malarije")
     } else if (input$bolezen == "sifilis") {
-      sif <- filter(bolezni, leto == n, bolezen == "sifilis")
+      sif <- filter(bolezni, leto == input$leto, bolezen == "sifilis")
       sif$bolezen <- NULL
       sif$leto <- NULL
       z <- ggplot() + geom_polygon(data = svet %>% left_join(sif, by = c("sovereignt" = "drzava")),
                                        aes(x = long, y = lat, group = group, color = "black", 
                                            fill = pojavnost)) + 
-        ggtitle("Zemljevid prikazuje razširjenost prirojenega sifilisa") 
+        ggtitle("Zemljevid prikazuje razširjenost prirojenega sifilisa") + 
+        labs(caption = "Opomba: Podatki o sifilisu so dostopni zgolj za leti 2012 in 2013.")
     } else {
-      tub <- filter(bolezni, leto == n, bolezen == "tuberkuloza")
+      tub <- filter(bolezni, leto == input$leto, bolezen == "tuberkuloza")
       tub$bolezen <- NULL
       tub$leto <- NULL
       z <- ggplot() + geom_polygon(data = svet %>% left_join(tub, by = c("sovereignt" = "drzava")),
@@ -48,4 +51,56 @@ shinyServer(function(input, output){
       guides(fill=guide_legend(title=NULL), color="none") + 
       scale_fill_gradientn(colours = c("blue", "green", "red"), 
                            values = rescale(c(0, 50, 200, 700)))
-  })})
+  })
+  
+  
+  output$znacilnosti <- renderPlot({
+    if (input$znacilnost == "dostopnost pitne vode") {
+      vo <- filter(znacilnosti, leto == input$leto, znacilnost == "voda") 
+      vo$znacilnost <- NULL
+      vo$leto <- NULL
+      z <- ggplot() + geom_polygon(data = svet %>% left_join(vo, by = c("sovereignt" = "drzava")),
+                                   aes(x = long, y = lat, group = group, color = "black", 
+                                       fill = pojavnost)) + 
+        ggtitle("Zemljevid prikazuje delež populacije z dostopom do izboljšanega pitnega vodnega vira ") + 
+        labs(caption = "Opomba: Izboljšan pitni vodni vir predstavlja zasebna in javna vodovodna napeljava, javni hidranti, zavarovani vodnjaki in izviri, zbrana deževnica.")
+    } else if (input$znacilnost == "zdravstvena potrošnja na prebivalca") { 
+      de <- filter(znacilnosti, leto == input$leto, znacilnost == "denar")
+      de$znacilnost <- NULL
+      de$leto <- NULL
+      z <- ggplot() + geom_polygon(data = svet %>% left_join(de, by = c("sovereignt" = "drzava")),
+                                   aes(x = long, y = lat, group = group, color = "black", 
+                                       fill = pojavnost)) + 
+        ggtitle("Zemljevid prikazuje zdravstveno potrošnjo na prebivalca (preračunano na mednarodni dolar, 2011)")
+    } else if (input$znacilnost == "stopnja podhranjenosti (5-)") { 
+      po <- filter(znacilnosti, leto == input$leto, znacilnost == "podhranjenost")
+      po$znacilnost <- NULL
+      po$leto <- NULL
+      z <- ggplot() + geom_polygon(data = svet %>% left_join(po, by = c("sovereignt" = "drzava")),
+                                   aes(x = long, y = lat, group = group, color = "black", 
+                                       fill = pojavnost)) + 
+        ggtitle("Zemljevid prikazuje prikazuje delež otrok, mlajših od pet let, s prenizko telesno težo")
+    }  else if (input$znacilnost == "stopnja debelosti (5-)") { 
+      de1 <- filter(znacilnosti, leto == input$leto, znacilnost == "debelost")
+      de1$znacilnost <- NULL
+      de1$leto <- NULL
+      z <- ggplot() + geom_polygon(data = svet %>% left_join(de1, by = c("sovereignt" = "drzava")),
+                                   aes(x = long, y = lat, group = group, color = "black", 
+                                       fill = pojavnost)) + 
+        ggtitle("Zemljevid prikazuje prikazuje delež otrok, mlajših od pet let, s previsoko telesno težo")
+    } else { 
+      al <- filter(znacilnosti, leto == input$leto, znacilnost == "alkohol")
+      al$znacilnost <- NULL
+      al$leto <- NULL
+      z <- ggplot() + geom_polygon(data = svet %>% left_join(al, by = c("sovereignt" = "drzava")),
+                                   aes(x = long, y = lat, group = group, color = "black", 
+                                       fill = pojavnost)) + 
+        ggtitle("Zemljevid prikazuje prikazuje porabo alkohola med osebami, starejšimi od 15 let, v litrih čistega alkohola")
+    } 
+    z + theme_void() + theme(plot.title = element_text(hjust = 0.5)) + 
+      guides(fill=guide_legend(title=NULL), color="none") + 
+      scale_fill_gradientn(colours = c("blue", "green", "red"), 
+                           values = rescale(c(0, 50, 200, 700)))
+  })
+  
+  })
--- shiny/ui.R
+++ shiny/ui.R
@@ -2,17 +2,45 @@ library(shiny)
 
 shinyUI(fluidPage(
   
-  titlePanel("Razširjenost nekaterih bolezni"),
+  titlePanel("Interaktivni prikaz"),
   
-  sidebarLayout(
-    sidebarPanel(("Izberi željene podatke:"), 
+  
+  tabsetPanel(
+    tabPanel("bolezni",
+             hr(),
+             sidebarLayout(
+               
+               sidebarPanel(("Izberi željene podatke:"), 
                  selectInput("bolezen", "Bolezen:", 
                              choices = c("AIDS", "kolera", "malarija", "sifilis", "tuberkuloza"),
                              multiple = FALSE),
-                 selectInput("leto", "Leto:", choices = 2000:2015, multiple = FALSE),
-                 actionButton("do", "Potrdi")
+                 selectInput("leto", "Leto:", choices = 2000:2015, multiple = FALSE)
                  ),
     
-    mainPanel(plotOutput("zemljevid")))
+    mainPanel(plotOutput("bolezni")))
   
+  )),
+
+  
+  tabsetPanel(
+    tabPanel("značilnosti",
+             hr(),
+             sidebarLayout(
+               
+               sidebarPanel(("Izberi željene podatke."),
+                            selectInput("znacilnost", "Značilnost:",
+                                        choices = c("dostopnost pitne vode", 
+                                                    "zdravstvena potrošnja na prebivalca",
+                                                    "stopnja podhranjenosti (5-)",
+                                                    "stopnja debelosti (5-)",
+                                                    "količina porabljenega alkohola na osebo (15+)"),
+                                        multiple = FALSE),
+                            selectInput("leto", "Leto:", choices = 2000:2015, multiple = FALSE)
+                            ),
+               
+               mainPanel(plotOutput("znacilnosti")))
+             
+    )
+  )
+
 ))
