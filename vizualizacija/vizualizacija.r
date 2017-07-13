aa70e8f900711e965138e46d11e6f73898461ab0
Nastja Kosir <nastja.kosir555@gmail.com>
2017-07-13 17:45:26 +0200

    brisanje oz. prestavitev nepotrebnega

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -19,10 +19,6 @@ g_mal1 <- ggplot(filter(bolezni, bolezni$bolezen == "malarija" )) +
   aes(x = leto, y = pojavnost/1000000, group = leto) + 
   geom_boxplot() + theme_bw() + labs(y="število okuženih z malarijo (v milijonih)")
 
-g_hiv <- ggplot(filter(bolezni, bolezni$bolezen == "hiv" )) + 
-  aes(x = leto, y = pojavnost, group = leto) + 
-  geom_boxplot() + theme_bw() + labs(y="delež okuženih z virusom hiv")
-
 # globalna poraba alkohola (15+)
 alko <- filter(znacilnosti, znacilnosti$znacilnost == "alkohol") %>% group_by(leto) %>% 
   summarise(povprecje = mean(pojavnost)) 
@@ -31,12 +27,7 @@ g_alk1 <- ggplot(alko) + aes(x=leto, y=povprecje) + geom_point() +
   labs(y="globalna poraba alkohola (15+) [liter čistega alkohola]")
 
 # delež kadilcev (15+)
-g_tb <- ggplot(tobak) + aes(x=leto, y=pojavnost, color=spol) + geom_jitter() + theme_bw() + 
-  labs(y="delež kadilcev (15+) [%]")
+slovarcek <- c("moski" = "moški", "zenske" = "ženski")
+g_tb <- ggplot(tobak) + aes(x=leto, y=pojavnost, color=slovarcek[spol]) + geom_jitter() + 
+  theme_bw() + labs(y="delež kadilcev (15+) [%]") + guides(color = guide_legend(title = "Spol"))
 #tob <- tobak %>% group_by(spol) %>% summarise(mean(pojavnost))
-
-
-#kv <- lm(data = alko, povprecje ~ leto + I(leto^2))
-#predict(kv, data.frame(leto=seq(2015, 2025, 1)))
-
-rm(alko)
