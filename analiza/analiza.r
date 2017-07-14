f4c08863623072a2415c79de6bccfe19cc3f8939
Nastja Kosir <nastja.kosir555@gmail.com>
2017-07-14 09:02:13 +0200

    popravki, brisanje nepotrebnega

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -3,9 +3,6 @@
 n <- 2010
 
 # države po skupinah glede na značilnosti
-alk <- filter(znacilnosti, leto == n, znacilnost == "alkohol") %>% rename(alkohol = pojavnost)
-alk$znacilnost <- NULL
-alk$leto <- NULL
 den <- filter(znacilnosti, leto == n, znacilnost == "denar") %>% rename(denar = pojavnost)
 den$znacilnost <- NULL
 den$leto <- NULL
@@ -13,55 +10,19 @@ vod <- filter(znacilnosti, leto == n, znacilnost == "voda") %>% rename(voda = po
 vod$znacilnost <- NULL
 vod$leto <- NULL
 
-zna <- inner_join(alk, den) %>% inner_join(vod)
-zna1<- zna
+zna <- inner_join(den, vod)
+zna1 <- zna
 row.names(zna) <- zna$drzava
 zna$drzava <- NULL
 
 sk <- scale(zna) %>% kmeans(5, nstart = 1000)
 skupine <- data.frame(drzava = zna1$drzava, skupina = factor(sk$cluster))
 g_zna <- ggplot() + geom_polygon(data = svet %>% left_join(skupine, by = c("sovereignt" = "drzava")),
-                        aes(x = long, y = lat, group = group, color = "black", fill = skupina)) + 
-  theme_void() + ggtitle("Države po podobnih značilnostih") + 
-  theme(plot.title = element_text(hjust = 0.5)) + 
-  guides(fill=guide_legend(title=NULL), color="none")
-
-zna0 <- inner_join(den, vod)
-zna10 <- zna0
-row.names(zna0) <- zna0$drzava
-zna0$drzava <- NULL
-
-sk0 <- scale(zna0) %>% kmeans(5, nstart = 1000)
-skupine0 <- data.frame(drzava = zna10$drzava, skupina0 = factor(sk0$cluster))
-g_zna0 <- ggplot() + geom_polygon(data = svet %>% left_join(skupine0, by = c("sovereignt" = "drzava")),
-                                 aes(x = long, y = lat, group = group, color = "black", fill = skupina0)) + 
+                                 aes(x = long, y = lat, group = group, color = "black", fill = skupina)) + 
   theme_void() + ggtitle("Države po podobnih značilnostih") + 
   theme(plot.title = element_text(hjust = 0.5)) + 
   guides(fill=guide_legend(title=NULL), color="none") +
   labs(caption="Opomba: Zemljevid prikazuje razporeditev glede na leto 2000.")
-
-
-# države po skupinah glede na razširjenost uporabe tobačnih izdelkov
-# zenske <- filter(tobak, leto == n, spol == "zenske") %>% rename(zenske = pojavnost)
-# zenske$spol <- NULL
-# zenske$leto <- NULL
-# moski <- filter(tobak, leto == n, spol == "moski") %>% rename(moski = pojavnost)
-# moski$spol <- NULL
-# moski$leto <- NULL
-# 
-# to <- inner_join(zenske, moski)
-# to1 <- to
-# row.names(to) <- to$drzava
-# to$drzava <- NULL
-# 
-# sk0 <- scale(to) %>% kmeans(5, nstart = 1000)
-# skupine0 <- data.frame(drzava = to1$drzava, skupina0 = factor(sk0$cluster))
-# g_tob <- ggplot() + geom_polygon(data = svet %>% left_join(skupine0, by = c("sovereignt" = "drzava")),
-#                         aes(x = long, y = lat, group = group, color = "black", 
-#                             fill = skupina0)) + 
-#   theme_void() + ggtitle("Države po podobni razširjenosti uporabe tobačnih izdelkov") + 
-#   theme(plot.title = element_text(hjust = 0.5)) + 
-#   guides(fill=guide_legend(title=NULL), color="none")
 
 
 kv <- lm(data = alko, povprecje ~ leto + I(leto^2)) %>% 
@@ -71,7 +32,4 @@ kv$leto <- 2016:2025
 kv <- kv[c(2,1)]
 
 
-rm(alk, den, vod)
-
-
-
+rm(den, vod, zna, zna1, sk, skupine)
